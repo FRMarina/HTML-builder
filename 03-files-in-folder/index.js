338dfaacd83d4e25c722f674feb16bc1f5a48816
Maryna Fedchik <marinafedchik@gmail.com>
2022-05-23 02:45:40 +0300

    feat: the third task was added

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs/promises');
+const {stat} = require('fs')
+const path = require('path');
+
+try{
+    const files = fs.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});
+    files.then(arrayOfFiles=>{
+        arrayOfFiles.forEach(async dirent=>{
+            if(dirent.isFile()){
+                const {name, ext} = path.parse(`${dirent.name}`);
+                stat(path.join(__dirname,'secret-folder',`${dirent.name}`), (err, stats) => {
+                      console.log(`${name} - ${ext} - ${stats.size}`);
+                });               
+            }
+        });
+    });
+}catch(err){
+    console.error(err);
+}
