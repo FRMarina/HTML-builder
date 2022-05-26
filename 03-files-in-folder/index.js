4d4db2199978c3fcfdf3e3d2eedbce0949a69aae
Maryna Fedchik <marinafedchik@gmail.com>
2022-05-26 23:52:50 +0300

    fix: fix 3rd task to delete point

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,7 +9,7 @@ try{
             if(dirent.isFile()){
                 const {name, ext} = path.parse(`${dirent.name}`);
                 stat(path.join(__dirname,'secret-folder',`${dirent.name}`), (err, stats) => {
-                      console.log(`${name} - ${ext} - ${stats.size}`);
+                      console.log(`${name} - ${ext.split('').splice(1,3).join('')} - ${stats.size}`);
                 });               
             }
         });
